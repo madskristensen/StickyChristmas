79c4eb2131674984eea978ce1253c208b4fe8f42
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-12-04 14:47:50 -0800

    Added logic

---
+++ src/StickyKeyHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Threading;
+using EnvDTE;
+using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+using Task = System.Threading.Tasks.Task;
+
+namespace StickyChristmas
+{
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof(StickyKeyHandler))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    public class StickyKeyHandler : ICommandHandler<TypeCharCommandArgs>
+    {
+        private CancellationTokenSource _source;
+        private bool _isStuck;
+        private DateTime _lastRun = DateTime.MinValue;
+
+        public string DisplayName => nameof(StickyKeyHandler);
+
+        public bool ExecuteCommand(TypeCharCommandArgs args, CommandExecutionContext executionContext)
+        {
+            if (ShouldRun(args.TypedChar))
+            {
+                _isStuck = true;
+                _source = new CancellationTokenSource(5000);
+
+                MakeKeyStickyAsync(_source.Token, args)
+                    .FileAndForget(nameof(Vsix.Name));
+            }
+            else
+            {
+                if (!_source.IsCancellationRequested)
+                {
+                    _source?.Dispose();
+                }
+
+                _isStuck = false;
+            }
+
+            return false;
+        }
+
+        private async Task MakeKeyStickyAsync(CancellationToken token, TypeCharCommandArgs args)
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var dte = await AsyncServiceProvider.GlobalProvider.GetServiceAsync<DTE, DTE>();
+
+            try
+            {
+                dte.UndoContext.Open(Vsix.Name);
+
+                while (!token.IsCancellationRequested)
+                {
+                    await Task.Delay(20);
+                    args.TextView.TextBuffer.Insert(args.TextView.Caret.Position.BufferPosition, args.TypedChar.ToString());
+                }
+            }
+            finally
+            {
+                dte.UndoContext.Close();
+                _lastRun = DateTime.Now;
+            }
+        }
+
+        private bool ShouldRun(char typedChar)
+        {
+            return _isStuck == false &&
+                   _lastRun < DateTime.Now.AddSeconds(-60);
+        }
+
+        public CommandState GetCommandState(TypeCharCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+    }
+}
